432cd01d5a78c516d8d5156b9cb0ca59d5aff1e1
momo <tell.me.something.mmk@gmail.com>
2025-02-13 21:21:22 +0900

    変更

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -28,11 +28,12 @@
         <div class="form__group">
           <div class="form__group-title">
             <span class="form__label--item">お名前</span>
-            <span class="form__label--required">必須</span>
+            <span class="form__label--required">※</span>
           </div>
           <div class="form__group-content">
             <div class="form__input--text">
-              <input type="text" name="name" placeholder="テスト太郎" />
+              <input type="varchar(255)" name="last_name" placeholder="例:山田" />
+              <input type="varchar(255)" name="first_name" placeholder="例:太郎" />
             </div>
             <div class="form__error">
               <!--バリデーション機能を実装したら記述します。-->
@@ -42,11 +43,11 @@
         <div class="form__group">
           <div class="form__group-title">
             <span class="form__label--item">性別</span>
-            <span class="form__larabel--required">必須</span>
+            <span class="form__larabel--required">※</span>
           </div>
           <div class="form__group-content">
             <div class="form__input--text">
-               <input type="gender" name="gender" />
+               <input type="gender" name="性別" />
             </div>
             <div class="form__error">
                 
@@ -56,11 +57,11 @@
         <div class="form__group">
           <div class="form__group-title">
             <span class="form__label--item">メールアドレス</span>
-            <span class="form__label--required">必須</span>
+            <span class="form__label--required">※</span>
           </div>
           <div class="form__group-content">
             <div class="form__input--text">
-              <input type="email" name="email" placeholder="test@example.com" />
+              <input type="varchar(255)" name="email" placeholder="例:test@example.com" />
             </div>
             <div class="form__error">
               <!--バリデーション機能を実装したら記述します。-->
@@ -70,11 +71,11 @@
         <div class="form__group">
           <div class="form__group-title">
             <span class="form__label--item">電話番号</span>
-            <span class="form__label--required">必須</span>
+            <span class="form__label--required">※</span>
           </div>
           <div class="form__group-content">
             <div class="form__input--text">
-              <input type="tel" name="tel" placeholder="09012345678" />
+              <input type="varchar(255)" name="tel" placeholder="08012345678" />
             </div>
             <div class="form__error">
               <!--バリデーション機能を実装したら記述します。-->
@@ -84,20 +85,28 @@
         <div class="form__group">
           <div class="form__group-title">
             <span class="form__label--item">住所</span>
-            <span class="form__label--required">必須</span>
+            <span class="form__label--required">※</span>
           </div>
           <div class="form__group-content">
             <div class="form__input--text">
-                <input type=>
-            </div>  
-            <div class="form__error">
-                
+                <input type="varchar(255)" name="address" placeholder="例:東京都渋谷区千駄ヶ谷1-2-3" />
             </div>
+           </div>       
           </div>
+        </div>
+        <div class="form_group">
+          <div class ="form__group-title">
+            <span class="form__label--item">建物名</span>    
+        </div> 
+        <div class="form__group-content">
+          <div class="form__input--text">
+               <input type="varchar(255)" name="building" placeholder="例:千駄ヶ谷マンション101" />
+          </div>
+         </div>
         </div>
         <div class="form__group">
           <div class="form__group-title">
-            <span class="form__label--item">お問い合わせ内容</span>
+            <span class="form__label--item">お問い合わせの種類</span>
           </div>
           <div class="form__group-content">
             <div class="form__input--textarea">
