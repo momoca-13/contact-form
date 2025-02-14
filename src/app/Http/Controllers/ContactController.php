8864bd5ff288eac1e5b3965bcad5cd158a796039
momo <tell.me.something.mmk@gmail.com>
2025-02-14 20:36:58 +0900

    変更

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Contact;
 
 class ContactController extends Controller
 {
@@ -14,5 +15,11 @@ class ContactController extends Controller
    {
     $contact = $request->only(['last_name', 'first_name', 'email', 'tel', 'address', 'building', 'content',]);
     return view('confirm', compact('contact'));
+   }
+   public function store(Request $request)
+   {
+    $contact = $request->only(['last_name', 'first_name', 'email', 'tel', 'address', 'building', 'content',]);
+    Contact::create($contact);
+    return view('thanks');
    }
 }
